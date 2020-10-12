f2e50c8858334386179086a6b831c29ced13d645
Zoe Lucinda <zoelucindagrassato@gmail.com>
2020-10-12 13:00:32 +0200

    progress

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 def workbook(n, k, arr):
+
+#here we determine how many pages will be needed per chapter
     index = 1
     dictionary_of_chapters = {}
     for item in arr:
@@ -12,8 +14,32 @@ def workbook(n, k, arr):
             dictionary_of_chapters[index] = 1
         index += 1
 
-def determine_if_contains_special_problems(k, number_of_problems, current_chapter):
+    determine_special_problems(k, sum(arr), dictionary_of_chapters, arr)
 
+
+
+#here we determine if the pages for the current_chapter contain any special problems
+def determine_special_problems(k, number_of_problems, chapters, arr):
+    total_special_problems = 0
+    index = 1
+
+    for i in range (0, len(chapters)):
+        num_of_problems_cur_chapter = arr[index - 1]
+        problems_left = num_of_problems_cur_chapter
+        num_of_pages_cur_chapter = chapters[index]
+
+        for j in range(1, num_of_pages_cur_chapter):
+            start_at = num_of_problems_cur_chapter - problems_left
+            select_load = 0
+            if j == num_of_pages_cur_chapter:
+                select_load = problems_left
+            else:
+                select_load = k + 1
+
+            sublist = [i for i in range(start_at, select_load)]
+            if index in sublist:
+                total_special_problems += 1
+            problems_left = problems_left - 3
 
 
 listOfItems = [4, 2, 6, 1, 10]
